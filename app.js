bb4744e154d0ffb185333ad6a1aa2b6502a16709
bambie669 <raduroman94@gmail.com>
2021-06-16 23:11:39 +0300

    playing with scope

---
+++ app.js
@@ -0,0 +1,23 @@
+var name = 'Radu';
+
+{
+ let test = 5;
+ console.log(test)
+}
+
+console.log(test)
+
+if (name ==='Radu') {
+    let hobbies = ['City building games', 'Tennis']
+    console.log(hobbies)
+}
+
+function greet() {
+    var age = 26;
+    var name = 'Bambie';
+    console.log(name, age, hobbies);
+}
+
+console.log(name, hobbies)
+
+greet();
